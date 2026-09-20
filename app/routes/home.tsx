5e29456712380c0c5d8cbdbfcd4bd24801e2ed46
mohit-vb <mohitvbjadhav@gmail.com>
2026-09-20 19:15:40 +0530

    chore : initial project configuration

---
+++ app/routes/home.tsx
@@ -0,0 +1,10 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: "ResuLens" }, { name: "description", content: "Welcome!" }];
+}
+
+export default function Home() {
+  return <Welcome />;
+}
