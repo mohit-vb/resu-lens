77aa5d18ab76be3aaeaaace0df438919ceb40384
mohit-vb <mohitvbjadhav@gmail.com>
2026-09-21 10:42:34 +0530

    feat : auth setup with puterjs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
 import type { Route } from "./+types/home";
 import { resumes } from "~/constants";
 import { Navbar, ResumeCard } from "~/components";
+import { usePuterStore } from "~/lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -10,6 +13,16 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!auth.isAuthenticated) navigate("/auth?next=/");
+    },
+    [auth.isAuthenticated],
+  );
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-no-repeat bg-center bg-cover">
       <Navbar />
